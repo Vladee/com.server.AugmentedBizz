3ec30d03f919665800b5dc0fbd41afc24d26ba02
Vladi Dexheimer <v_oi_d@hotmail.de>
2011-10-21 14:42:06 +0200

    initial commit

---
+++ AugmentedBizz/src/com/server/AugmentedBizzServlet.java
@@ -0,0 +1,12 @@
+package com.server;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class AugmentedBizzServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
